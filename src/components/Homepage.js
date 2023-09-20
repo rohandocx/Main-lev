f1565672b907b1b4ab26c82e453b9fddffae53f9
Rohan Singh <72556733+rohandocx@users.noreply.github.com>
2023-09-20 14:01:15 +0530

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 const Homepage = () => {
   return (
    <div class='container'>
-  <div class='container-child'>
+  
     <h1 id='heading'>Welcome</h1>
-  </div>
+ 
   <div class='container-child'>
     <a href='https://todo-leverag-lcuz26yr9-rohandocx.vercel.app/'>TASKBOARD</a>
   </div>
