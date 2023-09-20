4edca41597e27e5f38fc34fd2f88503abfd10e15
Rohan Singh <72556733+rohandocx@users.noreply.github.com>
2023-09-20 13:57:25 +0530

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom'
 
 const Homepage = () => {
   return (
-    <div className='container'>
-        <h1 id='heading'>Welcome</h1>
-      <div id='container-child1' className='container-chid'>
-       <a href='https://todo-leverag-lcuz26yr9-rohandocx.vercel.app/'>TASKBOARD</a>
-      </div>
-      <div id='container-child2' className='container-chid'>
-      <a href='https://calculator-leverag-jgms.vercel.app/'>CALCULATOR</a>
-      </div>
-      <div id='container-child3' className='container-chid'>
-      <a href='https://weather-leverg-jvweohbfk-rohandocx.vercel.app/'>WEATHER</a>
-      </div>
-    </div>
+   <div class='container'>
+  <div class='container-child'>
+    <h1 id='heading'>Welcome</h1>
+  </div>
+  <div class='container-child'>
+    <a href='https://todo-leverag-lcuz26yr9-rohandocx.vercel.app/'>TASKBOARD</a>
+  </div>
+  <div class='container-child'>
+    <a href='https://calculator-leverag-jgms.vercel.app/'>CALCULATOR</a>
+  </div>
+  <div class='container-child'>
+    <a href='https://weather-leverg-jvweohbfk-rohandocx.vercel.app/'>WEATHER</a>
+  </div>
+</div>
   )
 }
 
